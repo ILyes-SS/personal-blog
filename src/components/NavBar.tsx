7158d95b3f83f88acb6c8c728fb9242492ab7623
ilyes benmalti <benmaltiilyes@gmail.com>
2025-09-26 09:04:21 +0200

    feat: display navbar links conditionally

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,20 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { LogoutButton } from "./logout-button";
+import { prisma } from "@/db/prisma";
 
 const NavBar = async () => {
   const user = await getUser();
+  const isAuthor = (
+    await prisma.user.findUnique({
+      where: { email: user?.email },
+    })
+  )?.isAuthor;
+
   return (
     <nav className="flex items-center justify-around p-4 max-[400px]:text-sm">
       <Link href="/">Home</Link>
-      <Link href="/my-posts">My Posts</Link>
+      {isAuthor && <Link href="/my-posts">My Posts</Link>}
       {user ? (
         <LogoutButton />
       ) : (
