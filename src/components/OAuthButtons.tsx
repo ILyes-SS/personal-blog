5c88606911a67615682aad5e72908b04503dee3c
ilyes <benmaltiilyes@gmail.com>
2026-06-10 15:19:48 +0100

    fix: add callback URL for oauth

--- src/components/OAuthButtons.tsx
+++ src/components/OAuthButtons.tsx
@@ -13,7 +13,7 @@ export default function OAuthButtons() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${window.location.origin}`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
     if (error) throw error;
